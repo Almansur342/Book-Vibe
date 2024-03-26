f93873f999a3de49c72c12e0a8085e71e762ebfa
Almansur342 <abdullahalmansur2002@gmail.com>
2024-03-27 00:45:13 +0600

    Listed Books done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 const Banner = () => {
   return (
     <section className="dark:bg-gray-100 dark:text-gray-800 mb-16 rounded-lg max-w-6xl mx-auto">
 	<div className="container flex flex-col justify-center p-6 mx-auto py-7  lg:flex-row lg:justify-between">
 		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-xl xl:max-w-xl lg:text-left">
 			<h1 className="text-5xl font-bold leading-none sm:text-5xl mb-12">Books to freshen up your bookshelf</h1>
-			<div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+			<NavLink to="/Listed Books" className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
 				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded bg-[#23BE0A] text-[#FFFFFF]">View The List</a>
 				
-			</div>
+			</NavLink>
 		</div>
 		<div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
 			<img src="/banner.png" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
--- src/components/Read/Read.jsx
+++ src/components/Read/Read.jsx
@@ -1,37 +1,49 @@
 const Read = ({data}) => {
-  const {image,bookName,author,category,tags,publisher,yearOfPublishing,rating} = data || {};
+  const {image,bookName,author,category,tags,publisher,yearOfPublishing,rating,totalPages} = data || {};
   return (
-    <div className=" flex flex-col max-w-6xl mx-auto p-6 sm:flex-row border-2 mb-6">
-      <div className="flex fle space-x-2 sm:space-x-4">
+    <div className=" flex flex-col  max-w-6xl mx-auto p-6 sm:flex-row border-2 mb-6">
+      <div className="flex gap-8">
         <div className="bg-[#1313130D] p-10 rounded-lg">
-        <img className=" flex-shrink-0 object-cover  dark:border- rounded outline-none h-32 w-40 dark:bg-gray-500" src={image} alt="Polaroid camera" />
+        <img className=" flex-shrink-0 object-cover  dark:border- rounded outline-none h-40 w-40 dark:bg-gray-500" src={image} alt="Polaroid camera" />
         </div>
         
-        {/* <div className="flex flex-col justify-between w-full pb-4"> */}
+        <div className="flex flex-col justify-between w-full pb-4">
           <div className="flex justify-between w-full pb-2 space-x-2">
-            <div className="space-y-1">
-              <h3 className="text-lg font-semibold leading-snug sm:pr-8">{bookName}</h3>
-              <p className="text-sm dark:text-gray-600">By : {author}</p>
-              <div>
-                 <h1>Tag</h1>
-                 <p>{tags[0]}</p>
-                 <p>{tags[1]}</p>
-                 <div>
+            <div className="space-y-3">
+              <h3 className="text-2xl font-bold sm:pr-8 text-[#131313]">{bookName}</h3>
+              <p className="text-base text-[#131313CC]">By : {author}</p>
+              <div className="flex gap-5 items-center mb-8">
+                 <h1 className="text-[#131313] font-bold">Tag</h1>
+                 <p className="bg-[#23BE0A0D] font-medium text-[#23BE0A] text-base px-4 rounded-full p-2">{tags[0]}</p>
+                 <p className="bg-[#23BE0A0D] font-medium text-[#23BE0A] text-base px-4 rounded-full p-2">{tags[1]}</p>
+                 <div className="flex gap-1">
                    <img src="/location.png" alt="" />
-                   <p>Year of Publishing: {yearOfPublishing}</p>
+                   <p className="text-[#131313CC]">Year of Publishing: {yearOfPublishing}</p>
                  </div>
-                 <hr className="my-3" />
+               
               </div>
-              <div>
-                <p>Category: {category}</p>
-                <p> Rating: {rating}</p>
-                <p></p>
+              <div className="flex gap-7">
+                <div className="flex gap-2">
+                  <img src="/add.png" alt="" />
+                <h1 className="text-[#13131399]">Publisher: {publisher}</h1>
+                </div>
+                <div className="flex gap-1">
+                  <img src="/page.png" alt="" />
+                  <h1 className="text-[#13131399]">Page: {totalPages}</h1>
+                </div>
+                 
+              </div>
+              <hr className="" />
+              <div className="flex gap-5">
+                <p className="bg-[#328EFF26] font-medium text-[#328EFF] text-base px-4 rounded-full p-2">Category: {category}</p>
+                <p className="bg-[#FFAC3326] font-medium text-[#FFAC33] text-base px-4 rounded-full p-2"> Rating: {rating}</p>
+                <p className="bg-[#23BE0A] font-medium text-[#FFFFFF] text-lg px-4 rounded-full p-2">View Details</p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    // </div>
+     </div> 
   );
 };
 
